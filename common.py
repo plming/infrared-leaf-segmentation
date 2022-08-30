cfdc73f5c95d9c54a5978fc23f8dd2a73d828c2e
Kim Hyeonseok <kim010322@gmail.com>
2022-08-30 20:06:03 +0900

    refactor pyplot config

--- common.py
+++ common.py
@@ -1,11 +1,7 @@
 from array import array
-import plotly.express as px
+from numpy import genfromtxt
 import numpy as np
-import matplotlib as mpl
-
-mpl.rcParams['figure.dpi'] = 150
-mpl.rcParams['font.family'] = 'Apple SD Gothic Neo'
-
+import matplotlib.pyplot as plt
 
 def load_ir_in_dat(filename: str) -> np.ndarray:
     WIDTH = 160
@@ -19,6 +15,7 @@ def load_ir_in_dat(filename: str) -> np.ndarray:
     ir_array.frombytes(bytes)
 
     result = np.array(ir_array).reshape(HEIGHT, WIDTH)
+
     # TODO: remove magic number
     result = (result - 27315) / 100
     return result
@@ -26,23 +23,20 @@ def load_ir_in_dat(filename: str) -> np.ndarray:
 
 def load_ir_in_csv(filename: str) -> np.ndarray:
     WIDTH = 320
-    return np.genfromtxt('./plant-ir/121.csv', delimiter=',',
-                         skip_header=2, usecols=range(1, WIDTH+1))
+    return genfromtxt(filename, delimiter=',',
+                      skip_header=2, usecols=range(1, WIDTH+1))
 
 
-def show_image(img, title=""):
-    fig = px.imshow(img, title=title)
-    fig.show()
+def show_image(img, title="") -> None:
+    plt.imshow(img)
+    plt.title(title)
+    plt.show()
 
 
-def show_histogram(img):
-    fig = px.histogram(img.ravel())
-    fig.show()
-
-
-def get_average_tempeature(ir: np.ndarray, mask: np.ndarray):
+def get_average_tempeature(ir: np.ndarray, mask: np.ndarray) -> float:
     assert len(ir.shape) == 2
     assert len(mask.shape) == 2
+
     num_region_pixels = 0
     sum_temperature = 0
     for y, x in np.ndindex(ir.shape):
@@ -51,3 +45,9 @@ def get_average_tempeature(ir: np.ndarray, mask: np.ndarray):
             num_region_pixels += 1
 
     return sum_temperature / num_region_pixels
+
+
+def show_histogram(ir: np.ndarray, title: str) -> None:
+    plt.hist(ir.ravel(), bins=128)
+    plt.title(title)
+    plt.show()
