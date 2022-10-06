9c8d9e56b16a25a32b286b3d07f915dcfac932f1
Kim Hyeonseok <kim010322@gmail.com>
2022-10-06 15:48:58 +0900

    rebase

--- src/util.py
+++ src/util.py
@@ -1,40 +1,38 @@
 from array import array
+from typing import Any  # TODO: remove Any for static type checks
 from numpy import genfromtxt
 import numpy as np
-import matplotlib.pyplot as plt
+import os
 
 
-def load_ir_in_dat(filename: str) -> np.ndarray:
+def load_ir_in_dat(path: str) -> np.ndarray[Any, Any]:
+    assert os.path.splitext(path)[1] == '.dat'
+
     WIDTH = 160
     HEIGHT = 120
 
-    with open(filename, 'rb') as file:
+    with open(path, 'rb') as file:
         USHORT_BYTE = 2
         bytes = file.read(HEIGHT * WIDTH * USHORT_BYTE)
 
-    ir_array = array('H')
-    ir_array.frombytes(bytes)
+    ir_array = array('H', bytes)
 
     result = np.array(ir_array).reshape(HEIGHT, WIDTH)
 
-    # TODO: remove magic number
+    # TODO: fix magic number
     result = (result - 27315) / 100
     return result
 
 
-def load_ir_in_csv(filename: str) -> np.ndarray:
+def load_ir_in_csv(path: str) -> np.ndarray[Any, Any]:
+    assert os.path.splitext(path)[1] == '.csv'
+
     WIDTH = 320
-    return genfromtxt(filename, delimiter=',',
+    return genfromtxt(path, delimiter=',',
                       skip_header=2, usecols=range(1, WIDTH+1))
 
 
-def show_image(img, title="") -> None:
-    plt.imshow(img)
-    plt.title(title)
-    plt.show()
-
-
-def get_average_tempeature(ir: np.ndarray, mask: np.ndarray) -> float:
+def get_average_tempeature(ir: np.ndarray[Any, Any], mask: np.ndarray[Any, Any]) -> float:
     assert len(ir.shape) == 2
     assert len(mask.shape) == 2
 
@@ -46,9 +44,3 @@ def get_average_tempeature(ir: np.ndarray, mask: np.ndarray) -> float:
             num_region_pixels += 1
 
     return sum_temperature / num_region_pixels
-
-
-def show_histogram(single_channel_img: np.ndarray, title: str) -> None:
-    plt.hist(single_channel_img.ravel(), bins='auto')
-    plt.title(title)
-    plt.show()
