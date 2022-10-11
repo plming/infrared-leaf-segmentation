977e02970e0d9c1cb18033795dc2c254bdd94d99
Kim Hyeonseok <kim010322@gmail.com>
2022-10-11 15:04:53 +0900

    refactor demo_ginseng

--- util.py
+++ util.py
@@ -1,11 +1,14 @@
-from array import array
-from typing import Any  # TODO: remove Any for static type checks
-from numpy import genfromtxt
-import numpy as np
 import os
+from array import array
+
+import cv2
+import jenkspy
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import genfromtxt, ndarray
 
 
-def load_ir_in_dat(path: str) -> np.ndarray[Any, Any]:
+def load_ir_in_dat(path: str) -> ndarray:
     assert os.path.splitext(path)[1] == '.dat'
 
     WIDTH = 160
@@ -13,34 +16,69 @@ def load_ir_in_dat(path: str) -> np.ndarray[Any, Any]:
 
     with open(path, 'rb') as file:
         USHORT_BYTE = 2
-        bytes = file.read(HEIGHT * WIDTH * USHORT_BYTE)
+        byte_stream = file.read(HEIGHT*WIDTH*USHORT_BYTE)
 
-    ir_array = array('H', bytes)
+    ir_array = array('H', byte_stream)
 
     result = np.array(ir_array).reshape(HEIGHT, WIDTH)
 
     # TODO: fix magic number
-    result = (result - 27315) / 100
+    result = (result - 27315)/100
     return result
 
 
-def load_ir_in_csv(path: str) -> np.ndarray[Any, Any]:
+def load_ir_in_csv(path: str) -> ndarray:
     assert os.path.splitext(path)[1] == '.csv'
 
     WIDTH = 320
-    return genfromtxt(path, delimiter=',',
-                      skip_header=2, usecols=range(1, WIDTH+1))
+    result = genfromtxt(path, delimiter=',', skip_header=2, usecols=range(1, WIDTH + 1))
+    assert result.shape == (320, 240)
+    return result
 
 
-def get_average_tempeature(ir: np.ndarray[Any, Any], mask: np.ndarray[Any, Any]) -> float:
-    assert len(ir.shape) == 2
-    assert len(mask.shape) == 2
+def show_ir(ir: ndarray) -> None:
+    plt.imshow(ir)
+    plt.show()
 
-    num_region_pixels = 0
-    sum_temperature = 0
-    for y, x in np.ndindex(ir.shape):
-        if mask[y, x] != 0:
-            sum_temperature += ir[y, x]
-            num_region_pixels += 1
 
-    return sum_temperature / num_region_pixels
+def show_rgb(rgb: ndarray) -> None:
+    plt.imshow(rgb)
+    plt.show()
+
+
+def get_excess_green(rgb: ndarray) -> ndarray:
+    exg = np.zeros(shape=rgb.shape[:-1])
+
+    for row, col, _channel in np.ndindex(rgb.shape):
+        b, g, r = rgb[row][col]/rgb[row][col].sum()
+        exg[row][col] = 2*g - r - b
+
+    return exg
+
+
+def load_rgb_in_jpeg(path: str) -> ndarray:
+    WIDTH = 160
+    HEIGHT = 120
+
+    rgb_image = cv2.imread(path)
+    rgb_image = cv2.resize(rgb_image,
+                           dsize=(WIDTH, HEIGHT),
+                           interpolation=cv2.INTER_AREA)
+
+    return rgb_image
+
+
+def get_average_temperature(ir: ndarray, mask: ndarray) -> float:
+    assert len(ir.shape) == 2 and len(mask.shape) == 2
+    average_per_channels = cv2.mean(ir, mask)
+    return average_per_channels[0]
+
+
+def predict_with_jenks(arr: ndarray) -> ndarray:
+    breaks = jenkspy.jenks_breaks(arr.ravel(), nb_class=2)
+    return cv2.inRange(arr, lowerb=breaks[1], upperb=breaks[2])
+
+
+"""
+ir -> leaf -> temperature
+"""
