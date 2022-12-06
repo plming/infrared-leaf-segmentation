cb5ed688777f7e8d6eaf6824613abc5eb8c4e931
Kim Hyeonseok <kim010322@gmail.com>
2022-12-06 11:39:40 +0900

    Refactoring OtsuModel

--- src/otsu_model.py
+++ src/otsu_model.py
@@ -1,4 +1,4 @@
-import cv2
+from cv2 import threshold, THRESH_BINARY, THRESH_OTSU
 
 from src.ir_image import IrImage
 from src.model import Model
@@ -6,5 +6,9 @@ from src.model import Model
 
 class OtsuModel(Model):
     def predict(self, x: IrImage):
-        _thresh, mask = cv2.threshold(x.image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        _thresh, mask = threshold(x.image,
+                                  thresh=None,
+                                  maxval=255,
+                                  type=THRESH_BINARY + THRESH_OTSU)
+
         return mask.astype(bool)
