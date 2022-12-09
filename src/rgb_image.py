50fe1a41ce358d3ec775cb2e94f7212a06d23e12
Kim Hyeonseok <kim010322@gmail.com>
2022-12-10 02:15:15 +0900

    vectorize exg computation

--- src/rgb_image.py
+++ src/rgb_image.py
@@ -1,7 +1,8 @@
-from numpy.typing import NDArray
 import numpy as np
-from numpy import uint8, bool8
 from jenkspy import jenks_breaks
+from numpy import uint8, bool_
+from numpy.typing import NDArray
+
 import src.util as util
 
 
@@ -16,20 +17,15 @@ class RgbImage:
         return self._rgb.copy()
 
     @property
-    def label(self) -> NDArray[bool8]:
-        # region compute excess green
-        exg = np.zeros(shape=self._rgb.shape[:-1])
+    def label(self) -> NDArray[bool_]:
+        WEIGHTS_PER_CHANNEL = np.array([-1, 2, -1])
+        weighted_sum_per_pixel = np.sum(self._rgb * WEIGHTS_PER_CHANNEL, axis=2)
 
-        for row, col, _channel in np.ndindex(self._rgb.shape):
-            rgb_sum = self._rgb[row][col].sum()
+        # replace 0 with 1 to avoid division by 0
+        sum_per_pixel = np.sum(self._rgb, axis=2)
+        sum_per_pixel[sum_per_pixel == 0] = 1
 
-            if rgb_sum == 0:
-                r, g, b = 0, 0, 0
-            else:
-                r, g, b = self._rgb[row][col] / rgb_sum
-
-            exg[row][col] = 2 * g - r - b
-        # endregion
+        exg = weighted_sum_per_pixel / sum_per_pixel
 
         breaks = jenks_breaks(exg.ravel(), n_classes=2)
         result = np.logical_and(exg >= breaks[1], exg <= breaks[2])
