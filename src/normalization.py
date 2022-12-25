44d73d8c68138ca696972bf8eb23ceec54420573
Kim Hyeonseok <kim010322@gmail.com>
2022-12-25 17:12:19 +0900

    Create normalization.py

---
+++ src/normalization.py
@@ -0,0 +1,13 @@
+from numpy.typing import NDArray
+import numpy as np
+
+
+def linear_scaling(x: NDArray, min_value: float, max_value: float) -> NDArray:
+    result = (x - np.min(x)) * (max_value - min_value) / (np.max(x) - np.min(x)) + min_value
+    assert (np.logical_and(result >= min_value, result <= max_value).all())
+
+    return result
+
+
+def standardization(x: NDArray) -> NDArray:
+    return (x - x.mean()) / x.std()
