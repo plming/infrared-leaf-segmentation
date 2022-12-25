d07a28bc8847225fbb623a619faa42d7a351b15d
Kim Hyeonseok <kim010322@gmail.com>
2022-12-25 17:12:42 +0900

    demo 파일 정리

--- demo/run_ginseng.py
+++ demo/run_ginseng.py
@@ -1,16 +1,26 @@
-import src.visualization as visualization
-from src.metric import intersection_over_union, pixel_accuracy
-from src.ginseng_rgb_image import GinsengRgbImage
+import time
+from typing import List
+
 from src.ir_image import IrImage
 from src.jenks_model import JenksModel
+from src.metric import intersection_over_union, pixel_accuracy
+from src.model import Model
+from src.otsu_model import OtsuModel
+from src.rgb_image import RgbImage
 
-ir = IrImage("./ginseng-ir/irimage_20220512_0800.dat")
-rgb = GinsengRgbImage("./ginseng-rgb/camimage_20220512_0800.jpg")
-model = JenksModel()
+start = time.time()
+ir = IrImage("../ginseng-ir/irimage_20220512_0800.dat")
+rgb = RgbImage("../ginseng-rgb/camimage_20220512_0800.jpg")
 
-print(f"정확도(IOU, %): {intersection_over_union(model.predict(ir), rgb.label) * 100:.2f}")
-print(f"정확도(PA, %): {pixel_accuracy(model.predict(ir), rgb.label) * 100:.2f}")
+models: List[Model] = [JenksModel(),
+                       OtsuModel(), ]
 
-visualization.show_image(ir.image, "ir")
-visualization.show_image(rgb.image, "rgb")
-visualization.show_image(model.predict(ir), "predicted")
+for model in models:
+    print(f"[{model.__class__.__name__}]")
+
+    predict = model.predict(ir)
+
+    print(f"IOU: {intersection_over_union(predict, rgb.label) * 100:.2f}%")
+    print(f"PA: {pixel_accuracy(predict, rgb.label) * 100:.2f}%")
+
+print(f"Time: {time.time() - start:.2f}s")
